182fa68691b6bf0a6f74359125c164e02df0c744
roobinidevi <31208856+roobinidevi@users.noreply.github.com>
2017-10-23 11:00:40 +0530

    add usersearch and usertypessearch in common/models

---
+++ common/models/UserTypesSearch.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace common\models;
+
+use common\models\UserTypes;
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+
+/**
+ * UserTypesSearch represents the model behind the search form about `common\models\UserTypes`.
+ */
+class UserTypesSearch extends UserTypes
+{
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['user_type_id', 'visible_site', 'reorder_notify', 'status', 'created_at', 'updated_at', 'created_by', 'updated_by', 'deleted_at'], 'integer'],
+            [['type_name', 'type_code'], 'safe'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+        $query = UserTypes::find();
+
+        // add conditions that should always apply here
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to return any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        // grid filtering conditions
+        $query->andFilterWhere([
+            'user_type_id' => $this->user_type_id,
+            'visible_site' => $this->visible_site,
+            'reorder_notify' => $this->reorder_notify,
+            'status' => $this->status,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+            'created_by' => $this->created_by,
+            'updated_by' => $this->updated_by,
+            'deleted_at' => $this->deleted_at,
+        ]);
+
+        $query->andFilterWhere(['like', 'type_name', $this->type_name])
+            ->andFilterWhere(['like', 'type_code', $this->type_code]);
+
+        return $dataProvider;
+    }
+}
