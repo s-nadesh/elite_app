625531a513228f6efb95a097cc6229ffcef4cd72
sumanas-1 <sumanas-1@localhost>
2017-11-11 12:45:21 +0530

    Merge origin/dev1 into dev1
    Conflicts:
    	backend/controllers/SiteController.php

--- backend/controllers/SiteController.php
+++ backend/controllers/SiteController.php
@@ -16,7 +16,7 @@ class SiteController extends Controller {
     
     public function test(){
              return 'Nadeshss';
-        return 'Nadeshss';
+       
     }
     /**
      * @inheritdoc
